f86965a52bcf7f74da798fce7116fdb2aba1d9c3
Joe Wagner <n.joseph.wagner@gmail.com>
2021-05-21 22:03:17 -0600

    Fix export of helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,19 +1,17 @@
 
-export default {
-  base64ToArray: function (value) {
-    // convert user input string to all characters that can be base64 encoded
-    const codeUnits = new Uint16Array(value.length);
-    for (let i = 0; i < codeUnits.length; i++) {
-      codeUnits[i] = value.charCodeAt(i);
-    }
-    const base64 = String.fromCharCode(...new Uint8Array(codeUnits.buffer));
-
-    // base64 encode the string
-    const len = base64.length;
-    let bytes = new Uint8Array(len);
-    for (let i = 0; i < len; i++) {
-        bytes[i] = base64.charCodeAt(i);
-    }
-    return bytes;
+export function base64ToArray(value) {
+  // convert user input string to all characters that can be base64 encoded
+  const codeUnits = new Uint16Array(value.length);
+  for (let i = 0; i < codeUnits.length; i++) {
+    codeUnits[i] = value.charCodeAt(i);
   }
-};
+  const base64 = String.fromCharCode(...new Uint8Array(codeUnits.buffer));
+
+  // base64 encode the string
+  const len = base64.length;
+  let bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+      bytes[i] = base64.charCodeAt(i);
+  }
+  return bytes;
+}
